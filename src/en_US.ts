227b46726a7d2071dc6b73019aaffbe4fbfa2d21
Lio Young <lio@werewolf.design>
2021-04-08 21:58:56 +0200

    add INSUFFICIENT_PERMISSIONS string

--- src/en_US.ts
+++ src/en_US.ts
@@ -1,3 +1,4 @@
 export default {
-    CHANNEL_NOT_NSFW: "This Channel is not marked as NSFW, please mark it as such an rerun this Command."
+    CHANNEL_NOT_NSFW: "This Channel is not marked as NSFW, please mark it as such an rerun this Command.",
+    INSUFFICIENT_PERMISSIONS: `You need \`PERMISSIONS\` Permissions to run this Command `
 }
