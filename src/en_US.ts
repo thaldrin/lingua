12732c2a429cef84450c17d31b165d452c4fc926
Lio Young <lio@werewolf.design>
2021-04-09 14:49:25 +0200

    add cooldown strings

--- src/en_US.ts
+++ src/en_US.ts
@@ -1,4 +1,6 @@
 export default {
     CHANNEL_NOT_NSFW: "This Channel is not marked as NSFW, please mark it as such an rerun this Command.",
-    INSUFFICIENT_PERMISSIONS: `You need \`PERMISSIONS\` Permissions to run this Command `
+    INSUFFICIENT_PERMISSIONS: `You need \`PERMISSIONS\` Permissions to run this Command `,
+    ON_COOLDOWN: `\`COMMAND\` is on Cooldown.`,
+    ON_COOLDOWN_DESCRIPTION: `\`COMMAND\` is has a cooldown of \`COOLDOWN\` \n Wait \`TIME\` berore trying to use it again.`
 }
