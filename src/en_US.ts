02df423f92bf5353fb8c5542526bece55c8d6379
Lio Young <lio@werewolf.design>
2021-04-22 03:38:53 +0200

    add tag strings

--- src/en_US.ts
+++ src/en_US.ts
@@ -1,3 +1,9 @@
 export default {
-    CHANNEL_NOT_NSFW: "This Channel is not marked as NSFW, please mark it as such an rerun this Command."
+    CHANNEL_NOT_NSFW: "This Channel is not marked as NSFW, please mark it as such an rerun this Command.",
+    INSUFFICIENT_PERMISSIONS: `You need \`PERMISSIONS\` Permissions to run this Command `,
+    ON_COOLDOWN: `\`COMMAND\` is on Cooldown.`,
+    ON_COOLDOWN_DESCRIPTION: `\`COMMAND\` has a cooldown of \`COOLDOWN\` \n Wait \`TIME\` berore trying to use it again.`,
+    ON_ERROR: `\`COMMAND\` encountered an Error.`,
+    NOT_ENOUGH_TAGS: `Not Enough Tags`,
+    NOT_ENOUGH_TAGS_DESC: "You need to provide Tags for me to Search for."
 }
