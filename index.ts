dc7c00d505d030d7ae6891d6649087191373a8d7
Lio Young <lio@werewolf.design>
2021-07-09 11:46:45 +0200

    rewrite everything with yaml

--- index.ts
+++ index.ts
@@ -1,7 +1,31 @@
-import en_US from "./src/en_US";
-import de_DE from "./src/de_DE";
+// import en_US from "./src/en_US";
+// import de_DE from "./src/de_DE";
 
-export default {
-    en_US,
-    de_DE
+// export default {
+//     en_US,
+//     de_DE
+// }
+
+// export default class Lingua {
+//     constructor() {
+
+//     }
+// }
+
+import jsyaml from 'js-yaml'
+// import yaml from 'yaml'
+
+import fs from 'fs'
+
+
+
+try {
+    const file1 = fs.readFileSync('./langs/1.yml', 'utf8')
+    const file2 = fs.readFileSync('./langs/2.yml', 'utf8')
+    const Doc = jsyaml.loadAll(file1)
+
+    console.log(Doc)
+} catch (error) {
+    console.log(error)
+
 }
--- src/en_US.ts
+++ src/en_US.ts
@@ -7,14 +7,9 @@ export default {
         ]
 
     },
-    CHANNEL_NOT_NSFW: "This Channel is not marked as NSFW, please mark it as such and rerun this Command.",
-    INSUFFICIENT_PERMISSIONS: `You need \`PERMISSIONS\` Permissions to run this Command `,
-    ON_COOLDOWN: `\`COMMAND\` is on Cooldown.`,
-    ON_COOLDOWN_DESCRIPTION: `\`COMMAND\` has a cooldown of \`COOLDOWN\` \n Wait \`TIME\` before trying to use it again.`,
-    ON_ERROR: `\`COMMAND\` encountered an Error.`,
     MISSING: {
         NOT_ENOUGH_TAGS: `Not Enough Tags`,
-        NOT_ENOUGH_TAGS_DESC: "You need to provide Tags for me to search for.",
+        NOT_ENOUGH_TAGS_DESC: "You need to provide a few Tags for me to search for.",
         RP_REQUIRE_MENTION: `You need to mention another user.`,
         MISSING_TITLE: `Missing title. Separate the title and description with a pipe (\`|\`).`,
         VALUE: {
@@ -23,56 +18,7 @@ export default {
             SETTING: "No Setting was given."
         }
     },
-    USER_INFO: {
-        INFO_ON: "Info on USER",
-        STATUS: "Status | STATE",
-        ROLES: "Roles [AMOUNT]",
-        USERNAME: "Username",
-        USERID: "User ID",
-        CREATED_DATE: "Created",
-        JOINED_AT: "Joined",
-        STATES: {
-            ONLINE: "Online",
-            DND: "Do not Disturb",
-            IDLE: "Idle",
-            OFFLINE: "Offline"
-        }
-    },
-    NOT_ENOUGH_TAGS_DESC: "You need to provide Tags for me to search for.",
-    NOT_ENOUGH_TAGS: `Not Enough Tags`,
-    RP_REQUIRE_MENTION: `You need to mention another user.`,
     RP_SELF: `Don't you want to ACTION someone other than yourself?`,
     RP_ME: `Don't ACTION me! ACTION someone else.`,
-    MISSING_TITLE: `Missing title. Separate the title and description with a pipe (\`|\`).`,
-    VALUE_SENT: "VALUE sent.",
-    INVITE_STRING: "You can invite BOT using this Link",
-    ROLL: ":game_die: Rolling AMOUNT DICE...",
-    CATEGORIES: {
-        INFO: { name: "Info", desc: "" },
-        NSFW: { name: ":underage: NSFW", desc: "Horny Stuff, obviously." },
-        ANIMALS: { name: "Animals", desc: "Cute Pictures of Animals to brighten your Day!" },
-        MISC: { name: "Misc", desc: "Miscellaneous Commands" },
-        FUN: { name: "Fun", desc: "" },
-        DEVELOPER: { name: "Developer", desc: "Developer only Commands, duh" },
-    },
-    DEVELOPER_ONLY: "This Command is only for Developers.",
-    MISC: {
-        ENABLED: "Enabled",
-        DISABLED: "Disabled",
-    },
-    LOCALE: {
-        LANGUAGE: "Language",
-        CONTRIBUTORS: "AMOUNT Translator(s)",
-        TITLE: "LANGUAGE by TRANSLATOR",
-        COMMAND_DESC: "BOT is available in AMOUNT Languages, you can easily change BOT's current Language by running `PREFIX locale set <country_code>`\n\n[Translations](https://t8.pm/lingua) are provided by the Community",
-        UPDATED_SETTING: 'Updated SETTING to VALUE',
-        UNSUPPORTED: "BOT does not support VALUE at this Time.",
-        DEFAULT: {
-            LANGUAGES: "Languages",
-            LOCALIZATION: "Localization",
-            CURRENT: `Current Language`,
-            SUB: `• Use \`PREFIX locale set <country_code>\` to set the Guild's Language`
-        }
-    }
 
 }
